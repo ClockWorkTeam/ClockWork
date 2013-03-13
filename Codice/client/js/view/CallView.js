28835171311b617123cd540a2920f5766ee67e45
Liquid90 <g.bain@libero.it>
2013-03-13 16:52:26 +0100

    aggioranemnto codice

--- Codice/client/js/view/CallView.js
+++ Codice/client/js/view/CallView.js
@@ -13,3 +13,69 @@
  */
  
 //definizione delle dipendenze
+define([
+ 'jquery',
+ 'underscore',  
+ 'backbone',
+ 'text!templates/CallTemplate.html',
+], function($, _, Backbone, CallTemplate){
+  var CallView = Backbone.View.extend({
+    //si occupa di legare gli eventi ad oggetti del DOM
+    events:{
+    },
+	
+    el : $('#call'),
+	
+    //indica in quale parte del DOM gestirà 
+    template : _.template(CallTemplate),
+    
+    
+    //funzione di inizializzazione dell'oggetto
+    initialize: function(){
+	
+      _.bindAll(this, 'render');
+		
+    },
+    
+    //funzione che effettua la scrittura della struttura della pagina
+    render: function(){
+		if(this.startcall())
+		{
+			if(this.options.type=='video')
+			{	
+				//alert('prova');
+				$(this.el).html(this.template({video:'video'}));
+				navigator.webkitGetUserMedia({video:true, audio:true},
+				function(stream) {
+				video.src = window.webkitURL.createObjectURL(stream);
+				});
+				}
+			if(this.options.type=='audio')
+			{
+				navigator.webkitGetUserMedia({video:false, audio:true},
+				function(stream) {
+				video.src = window.webkitURL.createObjectURL(stream);
+				});
+				$(this.el).html(this.template({video:'audio'}));
+				}
+			}
+		else
+		{
+			return false;
+			}
+    },
+    
+    startcall : function()
+    {
+		return true;
+		}
+  });
+
+  CallView.prototype.close = function(){
+    this.remove();
+    this.unbind();
+  };
+
+  return CallView;
+
+});
