5246fddbeb301a0c5030eb5e96f6c08c468eb2a7
Jes <gava.mare@gmail.com>
2013-03-26 11:01:54 +0100

    aggiornato esito_dei_test (test di unità)
    sistemati path sulla ddp package dao

--- Codice/server/dao/RecordMessageDaoSQLTest.java
+++ Codice/server/dao/RecordMessageDaoSQLTest.java
@@ -9,26 +9,25 @@ import java.util.Vector;
 public class RecordMessageDaoSQLTest {
 	private JavaConnectionSQLite connection;
 	private UserList usersList;
-	
 	private RecordMessageDaoSQL rec;
 	private UserDaoSQL users;
-	
+
 	private void init(){
 		connection =new JavaConnectionSQLite();
 		usersList= new UserList();
-		
+
 		rec=new RecordMessageDaoSQL(connection,usersList);
 		users= new UserDaoSQL(connection,usersList);
 	}
-	
+
 	@Test
 	public void testCreate() {
 		init();
 		User user1 =users.createUser("Liquid90", "ciao", "Giacomo", "Bain", "0.0.0.2");
 		User user2=users.createUser("Leo", "ciaociao", "Pardis", "ZH", "0.0.1.2");
-		
+
 		RecordMessage mex = rec.createMessage(user1.getUsername(), user2.getUsername(), "ciao", new Date(2013-3-12));
-		
+
 		Vector<RecordMessage> messages = user1.getMessages();
 		assertTrue("numero errato di messaggi presenti", messages.size()==0);
 		messages = user2.getMessages();
@@ -41,13 +40,13 @@ public class RecordMessageDaoSQLTest {
 		init();
 		User user1 =users.createUser("Liquid90", "ciao", "Giacomo", "Bain", "0.0.0.2");
 		User user2=users.createUser("Leo", "ciaociao", "Pardis", "ZH", "0.0.1.2");
-		
+
 		rec.createMessage(user1.getUsername(), user2.getUsername(), "ciao", new Date(2013-3-12));
-		
+
 		Vector<RecordMessage> messages = user2.getMessages();
 		assertTrue("Il messaggio non è stato assegnato correttamente", messages==rec.getMessages(user2.getUsername()));
 	}
-	
+
 	@Test
 	public void testRM(){
 		init();
