a73b1a1ccde82464765cb50db5edd11024e94604
alabruna <agostino.labruna@gmail.com>
2013-03-27 10:54:03 +0100

    aggiunta connessione al client

---
+++ Codice/client/js/connection.js
@@ -0,0 +1,24 @@
+/*
+ * 
+ *  Nome: {nome del file}
+ *  Package: {package di appartenenza}
+ *  Autore: {autore del file}
+ *  Data: {data di creazione del file}
+ *  Versione: {versione del file}
+ * 
+ *  Modifiche:
+ *  +---------+---------------+------------------+
+ *  | Data | Programmatore | Modifiche |
+ *  +---------+---------------+------------------+
+ *  | AAMMGG | NomeCognome | - [label]metodo1 |
+ *  | | | - [label]metodo2 |
+ *  | | | - ....           |
+ *  +---------+---------------+------------------+
+ * 
+ * 
+ */
+
+define(function(){
+  var wsc = new WebSocket('ws://127.0.0.1:8787');
+  return wsc;
+});
