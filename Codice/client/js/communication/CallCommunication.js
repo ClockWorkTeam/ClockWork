c4149f4ce407b1e1e9e9b1b90bd293e66d8ec138
Liquid90 <g.bain@libero.it>
2013-03-29 16:23:04 +0100

    Callcommunication

--- Codice/client/js/communication/CallCommunication.js
+++ Codice/client/js/communication/CallCommunication.js
@@ -23,7 +23,7 @@ define(['connection'], function(Connection){
 		//messaggio di conferma di connessione sulla console
 		var credentials = { ip: iptocall , type:'call', calltype:typecall};
 		Connection.send(JSON.stringify(credentials));
-    this.receiveAnswer(typecall, iptocall, this);
+		this.receiveAnswer(typecall, iptocall, this);
 	};
 	
 	//funzione che si occupa di inviare una risposta all'utente che ha efettuato la chiamata
@@ -31,169 +31,189 @@ define(['connection'], function(Connection){
 	{
 		var credentials = { response: true, ip:iptoCall, type:'answeredcall' };
 		Connection.send(JSON.stringify(credentials));
-		alert(typecall);
 		this.startCall(iptoCall, false, typecall);
 	};
   
-  CallCommunication.prototype.receiveAnswer = function (typecall, iptocall, call)
+	CallCommunication.prototype.receiveAnswer = function (typecall, iptocall, call)
 	{
-		
-    Connection.addEventListener("message", onAnswer, false);
-    function onAnswer(evt){
-      var answer = JSON.parse(evt.data);
-      if(answer.type==='answeredCall'){
-		  var isCaller=true;
-		  if(answer.response==='true'){
-			call.startCall(iptocall, isCaller,typecall)
+		Connection.addEventListener("message", onAnswer, false);
+		function onAnswer(evt){
+			var answer = JSON.parse(evt.data);
+			if(answer.type==='answeredCall'){
+				var isCaller=true;
+				if(answer.response==='true'){
+					call.startCall(iptocall, isCaller,typecall)
+				}
 			}
-		  }
 		}
 	};
 	
 	//funzione che si occupa di inizializzare la chiamata
 	CallCommunication.prototype.startCall = function (iptocall, isCaller, typecall)
 	{
-    var sourcevid = document.getElementById('sourcevid');
-    var remotevid = document.getElementById('remotevid');
-    var localStream = null;
-    var peerConn = null;
-    var started = false;
-    var description=null;
+		var sourcevid = document.getElementById('sourcevid');
+		var remotevid = document.getElementById('remotevid');
+		var localStream = null;
+		var peerConn = null;
+		var started = false;
+		var description=null;
+		var logg = function(s) { console.log(s); };
+		// when PeerConn is created, send setup data to peer via WebSocket
+		function onSignal(message) {
+		  logg("Sending setup signal");
+		  Connection.send(message);
+		}
 
-    var logg = function(s) { console.log(s); };
+		// when remote adds a stream, hand it on to the local video element
+		function onRemoteStreamAdded(event) {
+			logg("Added remote stream");
+			remotevid.src = window.webkitURL.createObjectURL(event.stream);
+		}
 
-    // when PeerConn is created, send setup data to peer via WebSocket
-    function onSignal(message) {
-      logg("Sending setup signal");
-      Connection.send(message);
-    }
+		// when remote removes a stream, remove it from the local video element
+		function onRemoteStreamRemoved(event) {
+			logg("Remove remote stream");
+			remotevid.src = "";
+		}
 
-    // when remote adds a stream, hand it on to the local video element
-    function onRemoteStreamAdded(event) {
-    logg("Added remote stream");
-    remotevid.src = window.webkitURL.createObjectURL(event.stream);
-    }
-
-    // when remote removes a stream, remove it from the local video element
-    function onRemoteStreamRemoved(event) {
-    logg("Remove remote stream");
-    remotevid.src = "";
-    }
-
-    function createPeerConnection() {
-      var pcConfig = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
-    try {
-      logg("Creating peer connection");
-      peerConn = new webkitRTCPeerConnection(pcConfig);
-      peerConn.onicecandidate = onIceCandidate;
-    } catch (e) {
-      console.log("Failed to create PeerConnection, exception: " + e.message);
-      }
-    peerConn.addEventListener("addstream", onRemoteStreamAdded, false);
-    peerConn.addEventListener("removestream", onRemoteStreamRemoved, false)
-    }
+		function createPeerConnection() {
+			var pcConfig = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+			try {
+				logg("Creating peer connection");
+				peerConn = new webkitRTCPeerConnection(pcConfig);
+				peerConn.onicecandidate = onIceCandidate;
+			} catch (e) {
+				console.log("Failed to create PeerConnection, exception: " + e.message);
+			}
+			peerConn.addEventListener("addstream", onRemoteStreamAdded, false);
+			peerConn.addEventListener("removestream", onRemoteStreamRemoved, false)
+		}
+		
 
     // start the connection upon user request
-    function connect() {
-    if (!started && localStream ) {
-      
-      createPeerConnection();
-      if( isCaller){
-      logg('Adding local stream...');
-      started = true;
-      peerConn.createOffer(gotDescription);
-      isCaller=true;
+		function connect() {
+			if (!started && localStream ) {	  
+				createPeerConnection();	  
+				started = true;
+				peerConn.addStream(localStream);
+				logg('Adding local stream...');
+				peerConn.createOffer(gotDescription);
+			} else {
+				alert("Local stream not running yet.");
+			}
 		}
-    } else {
-      alert("Local stream not running yet.");
-    }
-    }
-    
+		
     // accept connection request
-    Connection.addEventListener("message", onMessage, false);
-    function onMessage(evt) {
-    var msg = JSON.parse(evt.data);
-    logg("RECEIVED: "+evt.data);
-    alert(msg.type);
-    if (msg.type==='offer' && !isCaller)
-    {
-      createPeerConnection();
-      peerConn.addStream(localStream);
-      peerConn.setRemoteDescription(new RTCSessionDescription(msg));
-      peerConn.createAnswer(gotDescription);
-    started = true;
-      }
-    if (msg.type==='answer' && isCaller)
-    {
-      peerConn.setRemoteDescription(new RTCSessionDescription(msg));
-      }
-    if (msg.type === 'candidate' && started) {
-      var candidate = new RTCIceCandidate({sdpMLineIndex:msg.label,
-                         candidate:msg.candidate});
-      peerConn.addIceCandidate(candidate);
-    }
-    if (!started && msg==='connect') {
-      createPeerConnection();
-      logg('Adding local stream...');
-      
-      peerConn.addStream(localStream);
-      started = true;
-    }
+		Connection.addEventListener("message", onMessage, false);
+		function onMessage(evt) {
+			logg("RECEIVED: "+evt.data);
+				
+			var msg = JSON.parse(evt.data);
+			if (msg.type==='offer' && !isCaller)
+			{	
+                
+                started = true;
+				if(typecall==='video')
+				{				
+					navigator.webkitGetUserMedia({video:true, audio:true},
+					function(stream) {
+						sourcevid.src = window.webkitURL.createObjectURL(stream);
+						createPeerConnection();
+						localStream=stream;	
+						
+						peerConn.addStream(localStream);
+						peerConn.setRemoteDescription(new RTCSessionDescription(msg));
+						peerConn.createAnswer(gotDescription);
+						
+					});
+				
+				}
+				if(typecall==='audio')
+				{
+					navigator.webkitGetUserMedia({video:false, audio:true},
+					function(stream) {
+						sourcevid.src = window.webkitURL.createObjectURL(stream);
+						createPeerConnection();
+						localStream=stream;
+						
+						peerConn.addStream(localStream);
+						peerConn.setRemoteDescription(new RTCSessionDescription(msg));
+						peerConn.createAnswer(gotDescription);
+						
+					});
+				}           
+			}
+			if (msg.type==='answer' && isCaller)
+			{
+				peerConn.setRemoteDescription(new RTCSessionDescription(msg));
+			}
+			if (msg.type ==='candidate' && started) {
+				
+				logg("STARTED TRUE");
+										logg('Adding candidate...');
+				var candidate = new RTCIceCandidate({sdpMLineIndex:msg.label,
+                candidate:msg.candidate});
+				peerConn.addIceCandidate(candidate);
+			}
+				
+				
+			
     // Message returned from other side
-    logg('Processing signaling message...');
-    }
+			logg('Processing signaling message...');
+		}
     
-    function gotDescription(desc) {
+		function gotDescription(desc) {
 		
-        peerConn.setLocalDescription(desc);
-        alert(iptocall);
-        var credentials={description: desc, ip:iptocall, type:'offer'};
-        Connection.send(JSON.stringify(credentials));
-      }
+			peerConn.setLocalDescription(desc);
+			
+			var descrip=JSON.stringify(desc);
+			var credentials={description : descrip, ip : iptocall, type : 'offer'};
+			Connection.send(JSON.stringify(credentials));
+		}
     
-    function onIceCandidate(event) {
-    if (event.candidate) {
-      sendMessage({type: 'candidate',
-      label: event.candidate.sdpMLineIndex,
-      id: event.candidate.sdpMid,
-      candidate: event.candidate.candidate});
-    } else {
-      console.log("End of candidates.");
-    }
-    }
+		function onIceCandidate(event) {
+			if (event.candidate) {
+			  var candidate=JSON.stringify({type : 'candidate',
+			  label: event.candidate.sdpMLineIndex,
+			  id: event.candidate.sdpMid,
+			  candidate: event.candidate.candidate});
+			  var credentials={cand : candidate, ip: iptocall, type : 'candidate'};
+					sendMessage(credentials);
+
+			} else {
+			  console.log("End of candidates.");
+			}
+		}
     
-    function sendMessage(message) {
-    var msgString = JSON.stringify(message);
-    console.log('C->S: ' + msgString);
-    Connection.send(msgString);
-    }
+		function sendMessage(message) {
+			var msgString = JSON.stringify(message);
+			console.log('C->S: ' + msgString);
+			Connection.send(msgString);
+		}
 		
-		if(typecall=='video')
+		if(isCaller && typecall=='video')
 		{			
 			
 			navigator.webkitGetUserMedia({video:true, audio:true},
 			function(stream) {
 				sourcevid.src = window.webkitURL.createObjectURL(stream);
-				localStream=stream;
-				connect();
-				peerConn.addStream(stream);
+				localStream=stream;		
+				connect(stream);
         
-				});
+			});
 			
-			}
+		}
 			
-		if(typecall=='audio')
+		if(isCaller && typecall=='audio')
 		{
 			navigator.webkitGetUserMedia({video:false, audio:true},
 			function(stream) {
 				sourcevid.src = window.webkitURL.createObjectURL(stream);
-				localStream=stream;
+				localStream=stream;	
 				connect();
-				peerConn.addStream(stream);
-        
-				});
+			});
 				
-			}
+		}
 	};
-return CallCommunication;	
+	return CallCommunication;	
 });
