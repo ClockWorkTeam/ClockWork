be92fdf0954298a7103871fecd6ea63cb8d0ec6e
Pardis89 <par.zoh@gmail.com>
2013-04-13 12:42:46 +0200

    inserite classi Launcher e ServerMyTalk

--- Codice/server/Launcher.java
+++ Codice/server/Launcher.java
@@ -17,12 +17,45 @@
 
 package server;
 
+import server.dao.*;
+import server.shared.*;
+import server.usermanager.*;
+
 public class Launcher {
+	private JavaConnectionSQLite javaconnectionSQLite;
+	private UserList userList;
+	private LoginDao loginDao;
+	private UserDao userDao;
+	private RecordMessageDao recordMessageDao;
+	private static AuthenticationManager authenticationManager=new AuthenticationManager();
+	private static UserManager userManager= new UserManager();
 	
 	private static Launcher launcher=null;
 	
+	/**costruttore della classe
+	 * 
+	 */
+	private Launcher(){
+		this.javaconnectionSQLite=new JavaConnectionSQLite();
+		this.userList=new UserList();
+		this.loginDao=new LoginDaoSQL(javaconnectionSQLite, userList);
+		this.userDao=new UserDaoSQL(javaconnectionSQLite, userList);
+		this.recordMessageDao= new RecordMessageDaoSQL(javaconnectionSQLite, userList);
+		authenticationManager.init(recordMessageDao, loginDao, userDao);
+		userManager.init(userDao, recordMessageDao);
+	}
+	
+	public static UserManager getUserMenager(){
+		return userManager;
+	}
+	public static AuthenticationManager getAuthenticationManager(){
+		return authenticationManager;
+	}
+	
 	public static void getServer(){
-		if(launcher==null)
-			crea();
-	}	
+		if(launcher==null){
+			launcher=new Launcher();
+		}
+	}
+
 }
