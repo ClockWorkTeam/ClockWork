9d3500502279c759639afae67fdf798837c4f151
Pardis89 <par.zoh@gmail.com>
2013-04-20 16:25:36 +0200

    UserListTest sistemato con struttura JUnit

--- Codice/server/src/server/shared/UserListTest.java
+++ Codice/server/src/server/shared/UserListTest.java
@@ -1,3 +1,20 @@
+/**
+* Nome: UserListTest
+* Package: server.shared
+* Autore: Zohouri Haghian Pardis
+* Data: 2013/03/05
+* Versione: 1.0
+*
+* Modifiche:
+* +---------+---------------+--------------------------+
+* | Data    | Programmatore |         Modifiche        |
+* +---------+---------------+--------------------------+
+* |  130305 |     ZHP       | + creazione documento	   |
+* |         |               |                          |
+* +---------+---------------+--------------------------+
+*
+*/
+
 package server.shared;
 
 import static org.junit.Assert.*;
@@ -5,23 +22,43 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class UserListTest {
-	UserList users;
+	UserList userList;
 	
 	private void init(){
-		users=new UserList();
+		userList=new UserList();
 	}
+
 	@Test
-	public void test() {
+	public void testAddUser() {
 		init();
-		User user= new User("Liquid90","Giacomo","Bain","0.0.10.10");
-		assertTrue("Errore nella registrazione", users.addUser(user));
-		assertFalse("Aggiunti due user con lo stesso username", users.addUser(user));
-		assertTrue("User non trovato", users.getUser(user.getUsername())==user );
-		assertTrue("Numero utenti errato",(users.getAllUsers()).size()==1);
-		assertTrue("Errore nella cancellazione", users.removeUser(user.getUsername()));
-		assertFalse("Cancellato user non esistenta", users.removeUser(user.getUsername()));
-		assertTrue("Numero utenti errato",(users.getAllUsers()).size()==0);
-	
+		User user= new User("ClockWork7","Clock Work", "Team", "7");
+		assertTrue("Aggiunta non riuscita",userList.addUser(user));
+		assertFalse("Aggiunti due user con lo stesso username", userList.addUser(user));
+	}
+
+	@Test
+	public void testGetUser() {
+		init();
+		User user= new User("ClockWork7","Clock Work", "Team", "7");
+		assertFalse("User presente erroneamente", userList.getUser(user.getUsername())==user );
+		userList.addUser(user);
+		assertTrue("User non presente", userList.getUser(user.getUsername())==user );
+	}
+
+	@Test
+	public void testGetAllUsers() {
+		testAddUser();
+		assertTrue("Numero user presenti corretto", userList.getAllUsers().size()==1 );
+		User user= new User("ClockWork","Clock Work", "Team", "7");
+		userList.addUser(user);
+		assertTrue("User non presente", userList.getAllUsers().contains(user) );
+	}
+
+	@Test
+	public void testRemoveUser() {
+		testAddUser();
+		assertTrue("Rimozione riuscita", userList.removeUser("ClockWork7"));
+		assertTrue("Risultato corretto",userList.getAllUsers().size()==0);
 	}
 
 }
