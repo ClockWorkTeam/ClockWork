12b173e9a768a8050206a60f6ca22282e8aeb88b
Pardis89 <par.zoh@gmail.com>
2013-04-30 12:42:07 +0300

    modifiche lun29

--- Codice/client/js/communication/ContactsCommunication.js
+++ Codice/client/js/communication/ContactsCommunication.js
@@ -24,20 +24,23 @@ define(['connection'], function(Connection){
 		  
 			Connection.onmessage = function(str){
 				var response = JSON.parse(str.data);
-				for(var i=0; i<response.size; i++){
-          var existing = view.collection.find(function(contact){return contact.get('username') === response['username'+i];});
-          if(!existing){
-            view.collection.create(
-              {username: response['username'+i], 
-                name: response['name'+i], 
-                surname: response['surname'+i],  
-              IP: response['IP'+i]}
-            );
-          } else {
-            existing.set('IP', response['IP'+i]);
-          }
+				if(response.type==='getContacts'){
+					for(var i=0; i<response.size; i++){
+						var existing = view.collection.find(function(contact){return contact.get('username') === response['username'+i];});
+						if(!existing){
+							view.collection.create(
+								{username: response['username'+i], 
+									name: response['name'+i], 
+									surname: response['surname'+i],  
+								IP: response['IP'+i]}
+							);
+						} else {
+							existing.set('IP', response['IP'+i]);
+						}
+					}
+				
+				view.contacts_view.render();
 				}
-				          view.contacts_view.render();
 			}	  					   
 		}
 	
