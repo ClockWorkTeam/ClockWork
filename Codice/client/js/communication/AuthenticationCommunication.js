a554fef22de683e8d6a7d78a4b4f00e0e712ae42
Pardis89 <par.zoh@gmail.com>
2013-06-03 16:01:07 +0200

    ultima versione authenticationCommunication

--- Codice/client/js/communication/AuthenticationCommunication.js
+++ Codice/client/js/communication/AuthenticationCommunication.js
@@ -1,6 +1,6 @@
 /*
  * Nome:AuthenticationCommunication.js
- * Package: 
+ * Package: communication
  * Autore:
  * Data:
  * Versione:
@@ -14,68 +14,64 @@
  
 //classe che comunica con il server per i dati che riguardano il login
 define(['connection'], function(Connection){
-  
-	return {
-	
+  return {	
     //metodo per controllare la correttezza delle credenziali inserite	
     checkCredentials: function(user, pass, callBacks, view) {
       var credentials = {
-				type: "login",
+        type: "login",
         username: user,
         password: pass
       };
       Connection.send(JSON.stringify(credentials));
-      
-			Connection.onmessage = function(str){
-				var response = JSON.parse(str.data);
-				if(response.type==="login"){				
-					if(response.answer === "true"){
-						callBacks.doLogin(user, pass, response, view);
-					}else if(response.answer === "false"){
-						alert("Login e username errate");
-					}
-				}
-			}
-		},
+      Connection.onmessage = function(str){
+        var response = JSON.parse(str.data);
+	if(response.type==="login"){				
+	  if(response.answer === "true"){
+	    callBacks.doLogin(user, pass, response, view);
+	  }else if(response.answer === "false"){
+	    alert("Login e username errate");
+	  }
+	}
+      }
+    },
 		
     signup: function(user, pass, name, surname, callBacks, view) {
-			var credentials = {
-				type: "signUp",
-				username: user,
-				password: pass,
-				name: name,
-				surname: surname
-			};  
-			Connection.send(JSON.stringify(credentials));
-			Connection.onmessage = function(str){
-				var response = JSON.parse(str.data);
-				if(response.type==="signUp"){
-					if(response.answer === "true"){
-						response.name=name;
-						response.surname=surname;
-						callBacks.doLogin(user, pass, response, view);
-					}else{ 
-						alert("Username non disponibile");
-					}
-				}
-			}
-		},
+      var credentials = {
+        type: "signUp",
+	username: user,
+	password: pass,
+	name: name,
+	surname: surname
+      };  
+      Connection.send(JSON.stringify(credentials));
+      Connection.onmessage = function(str){
+        var response = JSON.parse(str.data);
+	if(response.type==="signUp"){
+	  if(response.answer === "true"){
+	    response.name=name;
+	    response.surname=surname;
+	    callBacks.doLogin(user, pass, response, view);
+	  }else{ 
+	    alert("Username non disponibile");
+	  }
+	}
+      }
+    },
 		
-		logout:function(user){
-			var credentials = {
-				type: "logout",
-				username: user
-			};  
-			Connection.send(JSON.stringify(credentials));	
-			Connection.onmessage = function(str){
-				var response = JSON.parse(str.data);
-				if(response.type==="logout"){
-					if(response.answer == "false"){
-						alert("Logout fallito");
-					}
-				}
-
-			}
-		}
-	};
+    logout:function(user){
+      var credentials = {
+	type: "logout",
+	username: user
+      };  
+      Connection.send(JSON.stringify(credentials));	
+      Connection.onmessage = function(str){
+	var response = JSON.parse(str.data);
+	if(response.type==="logout"){
+  	  if(response.answer == "false"){
+	    alert("Logout fallito");
+	  }
+	}
+      }
+    }
+  };
 });
