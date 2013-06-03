7532491b7006f79d5a17aeef1c24767c71079f1c
Pardis89 <par.zoh@gmail.com>
2013-06-03 16:32:03 +0200

    ultima versione contactsCommunication

--- Codice/client/js/communication/ContactsCommunication.js
+++ Codice/client/js/communication/ContactsCommunication.js
@@ -1,6 +1,6 @@
 /*
  * Nome: ContactsCommunication.js
- * Package: 
+ * Package: communication
  * Autore:
  * Data:
  * Versione:
@@ -13,35 +13,32 @@
  */
 //classe che comunica con il server per i dati che riguardano il ricavare la lista dei contatti
 define(['connection','collection/ContactsCollection'], function(Connection, contactsCollection){
-	
-	return {
-		fetchContacts: function(username) {
-			var request = {
-				type: 'getContacts',
-				username: username
-			};
-			Connection.send(JSON.stringify(request));
+  return {
+    fetchContacts: function(username) {
+      var request = {
+        type: 'getContacts',
+	username: username
+      };
+      Connection.send(JSON.stringify(request));
 		  
-			Connection.onmessage = function(str){
-				var response = JSON.parse(str.data);
-				if(response.type==='getContacts'){
-					for(var i=0; i<response.size; i++){
-						var existing = contactsCollection.find(function(contact){return contact.get('username') === response['username'+i];});
-						if(!existing){
-							contactsCollection.add(
-								{username: response['username'+i], 
-									name: response['name'+i], 
-									surname: response['surname'+i],  
-								IP: response['IP'+i]}
-							);
-						} else {
-							existing.set('IP', response['IP'+i]);
-						}
-					}
-
-				}
-			}	  					   
-		}
-	
-	};
+      Connection.onmessage = function(str){
+	var response = JSON.parse(str.data);
+	if(response.type==='getContacts'){
+  	  for(var i=0; i<response.size; i++){
+	    var existing = contactsCollection.find(function(contact){return contact.get('username') === response['username'+i];});
+	    if(!existing){
+	      contactsCollection.add(
+	        {username: response['username'+i], 
+	        name: response['name'+i], 
+	        surname: response['surname'+i],  
+	        IP: response['IP'+i]}
+	      );
+	    }else{
+              existing.set('IP', response['IP'+i]);
+	    }
+	  }
+        }
+      }	  					   
+    }
+  };
 });
