0326b523febee3e043baa92e1861a1fafae3cbdd
Pardis89 <par.zoh@gmail.com>
2013-06-04 10:46:50 +0200

    ultima versione chatVIew

--- Codice/client/js/view/ChatView.js
+++ Codice/client/js/view/ChatView.js
@@ -21,19 +21,19 @@ define([
  'text!templates/ChatTemplate.html',
  'collection/TextMessagesCollection'
 ], function($, _, Backbone, ChatCommunication, ChatTemplate, TextMessagesCollection){
-	var ChatView = Backbone.View.extend({
+  var ChatView = Backbone.View.extend({
     events:{
-			'click button#Send':'send',
-			'keyup #compose textarea':'pressEnter',
-		},
+      'click button#Send':'send',
+      'keyup #compose textarea':'pressEnter',
+    },
 	
-	  pressEnter:function(event){
-			if(event.keyCode == 13){
-				var val=(this.el).getElementsByTagName("textarea")[0].value;
-				(this.el).getElementsByTagName("textarea")[0].value=val.substring(0, val.length - 1);				
-				this.send();
-			}
-		},
+    pressEnter:function(event){
+      if(event.keyCode == 13){
+        var val=(this.el).getElementsByTagName("textarea")[0].value;
+	(this.el).getElementsByTagName("textarea")[0].value=val.substring(0, val.length - 1);				
+	this.send();
+      }
+    },
 	
     el : $("#chat"),
 
@@ -41,66 +41,62 @@ define([
   
     collection: TextMessagesCollection,
   
-		initialize: function(){
-			this.listenTo(this.collection, 'all', this.render);
-			_.bindAll(this, 'render', 'send');
-		},
+    initialize: function(){
+      this.listenTo(this.collection, 'all', this.render);
+      _.bindAll(this, 'render', 'send');
+    },
  
-		render: function(){	
-			if(this.options.userModel!=''){
-				$(this.el).html(this.chatTemplate({ip: this.model.toJSON().IP}));
-				this.putMessages();
-			}
-		},
+    render: function(){	
+      if(this.options.userModel!=''){
+	$(this.el).html(this.chatTemplate({ip: this.model.toJSON().IP}));
+	this.putMessages();
+      }
+    },
   
-		putMessages:function(){  
-			var messages=this.collection.chat_session(this.model.toJSON().username);
-			for(var i=0; i<messages.length; i++){
-					this.putMessage(messages[i]);
-			}
-		},
+    putMessages:function(){  
+      var messages=this.collection.chat_session(this.model.toJSON().username);
+      for(var i=0; i<messages.length; i++){
+        this.putMessage(messages[i]);
+      }
+    },
   
-		putMessage:function(TextMessageModel){
-			var node=document.createElement("LI");
-			var name=document.createElement("H3");
-			if(TextMessageModel.toJSON().source=='sent'){
-						name.appendChild(document.createTextNode(this.options.userModel.toJSON().username+": "));
-						node.setAttribute('class','sent');
-			}else if(TextMessageModel.toJSON().source=='received'){
-						name.appendChild(document.createTextNode(this.model.toJSON().username+": "));
-						node.setAttribute('class','received');				
-			}else if(TextMessageModel.toJSON().source=='notsent'){
-						name.appendChild(document.createTextNode(this.options.userModel.toJSON().username+": "));
-						node.setAttribute('class','notsent');				
-			}
-			var message=document.createTextNode(TextMessageModel.toJSON().message);
-			node.appendChild(name);
-			node.appendChild(message);
-			(this.el).getElementsByTagName("UL")[0].appendChild(node);
+    putMessage:function(TextMessageModel){
+      var node=document.createElement("LI");
+      var name=document.createElement("H3");
+      if(TextMessageModel.toJSON().source=='sent'){
+        name.appendChild(document.createTextNode(this.options.userModel.toJSON().username+": "));
+	node.setAttribute('class','sent');
+      }else if(TextMessageModel.toJSON().source=='received'){
+	name.appendChild(document.createTextNode(this.model.toJSON().username+": "));
+	node.setAttribute('class','received');				
+      }else if(TextMessageModel.toJSON().source=='notsent'){
+	name.appendChild(document.createTextNode(this.options.userModel.toJSON().username+": "));
+	node.setAttribute('class','notsent');				
+      }
+      var message=document.createTextNode(TextMessageModel.toJSON().message);
+      node.appendChild(name);
+      node.appendChild(message);
+      (this.el).getElementsByTagName("UL")[0].appendChild(node);
+    },
+  
+    //invia un messaggio
+    send:function(){
+      ChatCommunication.send(this.model.toJSON().username, (this.el).getElementsByTagName("textarea")[0].value);
+      this.collection.add({contact:this.model.toJSON().username, message:(this.el).getElementsByTagName("textarea")[0].value, source:'sent'});
+      (this.el).getElementsByTagName("textarea")[0].value='';
+    },
 		
-		},
-  
-		//invia un messaggio
-		send:function(){
-			ChatCommunication.send(this.model.toJSON().username, (this.el).getElementsByTagName("textarea")[0].value);
-			this.collection.add({contact:this.model.toJSON().username, message:(this.el).getElementsByTagName("textarea")[0].value, source:'sent'});
-			(this.el).getElementsByTagName("textarea")[0].value='';
-		},
-		
-		unrender:function(){
-			_.each(this.collection.chat_session(this.model.toJSON().username), function(message){message.clear();});
-			this.close();
-		}
+    unrender:function(){
+      _.each(this.collection.chat_session(this.model.toJSON().username), function(message){message.clear();});
+      this.close();
+    }
 
- });
+  });
  
-   ChatView.prototype.close = function(){
+  ChatView.prototype.close = function(){
     this.remove();
     this.unbind();
   };
 
-
- return ChatView;
+  return ChatView;
 });
-
-
