e84675eb69248972ff60eb753327d88afece0afb
Jes <gava.mare@gmail.com>
2013-06-06 10:08:06 +0200

    diario st sistemato

--- Codice/client/js/model/ContactModel.js
+++ Codice/client/js/model/ContactModel.js
@@ -4,7 +4,7 @@
  * Autore:
  * Data:
  * Versione:
- * 
+ *
  * Modifiche:
  * +------+---------------+-----------+
  * | Data | Programmatore | Modifiche |
@@ -16,7 +16,7 @@ define([
   'backbone'
   ], function(_, Backbone){
     var ContactModel = Backbone.Model.extend({
-	  
+
       defaults: {
         username: '',
         name: '',
@@ -24,7 +24,7 @@ define([
         IP: '0.0.0.0',
       	unread: 0
       }
-	    
+
     });
     return ContactModel;
 });
