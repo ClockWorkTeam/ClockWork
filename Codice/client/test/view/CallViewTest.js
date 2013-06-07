bbef033d95016e3c29f4c976771dd15be0e55b7f
Pardis89 <par.zoh@gmail.com>
2013-06-07 11:47:21 +0200

    template e bottoni CallView

---
+++ Codice/client/test/view/CallViewTest.js
@@ -0,0 +1,42 @@
+//Wait for relevant code bits to load before starting any tests
+define(['../js/view/CallView'], function( CallView ) {
+
+  module( 'About Backbone.View', {
+      setup: function() {
+        this.callView = new CallView();
+      },
+      teardown: function() {
+        this.callView.remove();
+      }
+  });
+
+  test('Should be tied to a DOM element when created, based off the property provided.', function() {
+    expect( 1 );
+    equal( this.callView.el.id.toLowerCase(), 'content', 'Tied to #content.' );
+  });
+ 
+  test('The template is render correctly.', function() {
+    expect( 2 );
+		
+		$(this.callView.el).html(this.callView.template({ username: 'clockwork' }));
+    // Check the number of items rendered
+    equal($('#content').find('video').length, 2, 'Two video frames rendered.');
+    equal($('#content').find('button').length, 1, 'One button rendered.');
+  });
+
+  test('Can wire up endCall method to DOM element.', function() {
+    expect( 1 );
+
+		$(this.callView.el).html(this.callView.template({ username: 'clockwork' }));
+    this.connectSpy = sinon.spy();
+    this.sendStub = sinon.stub(this.callView, 'endCall', this.connectSpy );
+    this.callView.delegateEvents();
+    // Trigger the event
+     $('button#endCall').click();
+    // Check the done status for the model is true
+    ok( this.connectSpy.called );
+    
+    this.sendStub.restore();
+  });
+
+});
