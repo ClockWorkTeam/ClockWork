e42c702c8a7eccc465e83565aef85a65781cdabd
Pardis89 <par.zoh@gmail.com>
2013-06-07 14:16:34 +0200

    chatView template e bottoni

---
+++ Codice/client/test/view/ChatViewTestConfig.js
@@ -0,0 +1,41 @@
+/*
+ * Nome:CallViewTestConfig.js
+ * Package: 
+ * Autore:
+ * Data:
+ * Versione:
+ * 
+ * Modifiche:
+ * +------+---------------+-----------+
+ * | Data | Programmatore | Modifiche |
+ * +------+---------------+-----------+
+ * |      |               | Scrittura codice          |
+ * |      |               | Inserite require  con le tre classi view iniziali |
+*/
+require.config({
+  baseUrl: '../../js/',
+  paths: {
+    jquery: '../test/resources/jquery/jquery-min',
+    underscore: '../test/resources/underscore/underscore-min',
+    backbone: '../test/resources/backbone/backbone',
+    text: '../test/resources/require/text',
+    storage: '../test/resources/backbone/backbone.noStorage'
+  },
+  
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+
+    backbone: {
+      deps: [ 'underscore', 'jquery' ],
+      exports: 'Backbone'
+    }
+  }
+});
+
+QUnit.config.autostart = false;
+ 
+require(['../view/ChatViewTest.js'], function(){
+  QUnit.start(); //Tests loaded, run tests
+});
