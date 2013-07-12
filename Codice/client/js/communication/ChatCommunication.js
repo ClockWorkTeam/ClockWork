19288aff10619c74ab119be43340c11c1f84f7c0
Pardis89 <par.zoh@gmail.com>
2013-07-12 12:48:36 +0200

    sistemato chatCommunication messaggi

--- Codice/client/js/communication/ChatCommunication.js
+++ Codice/client/js/communication/ChatCommunication.js
@@ -26,14 +26,14 @@ define(['connection', 'collection/ContactsCollection', 'collection/TextMessagesC
   function onReceived(str){
     var response = JSON.parse(str.data);
     if (response.type === 'sendText'){
-      TextMessagesCollection.add({contact:response.username, message:response.message ,source:'received'});
-      var currentUnread = ContactsCollection.where({username: response.username})[0].get("unread");
+      TextMessagesCollection.add({contact:response.contact, message:response.message ,source:'received'});
+      var currentUnread = ContactsCollection.where({username: response.contact})[0].get("unread");
       if (currentUnread > -1){
-        ContactsCollection.where({username: response.username})[0].set({unread: currentUnread + 1});
+        ContactsCollection.where({username: response.contact})[0].set({unread: currentUnread + 1});
       }
     }else if(response.type === 'notDelivered'){
-      alert('Messaggio per '+response.username+' non è stato consegnato');
-      var existing = TextMessagesCollection.find(function(mex){return (mex.get('contact') === response['username'] && mex.get('message')===response['message']);});
+      alert('Messaggio per '+response.contact+' non è stato consegnato');
+      var existing = TextMessagesCollection.find(function(mex){return (mex.get('contact') === response['contact'] && mex.get('message')===response['message']);});
       existing.set('source', 'notsent');
     }
   };
@@ -43,7 +43,7 @@ define(['connection', 'collection/ContactsCollection', 'collection/TextMessagesC
      * funzione per inviare il messaggio
      */
     send:function(contact, message){
-      var chatMessage = { type:'sendText', username: contact, message: message};
+      var chatMessage = { type:'sendText', contact: contact, message: message};
       Connection.send(JSON.stringify(chatMessage));
     }  
   };
