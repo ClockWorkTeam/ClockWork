beabf8e888c6b16f32b925c86d4a6c35380da780
vfurlan <valentino_furlan@yahoo.it>
2013-07-12 17:24:27 +0200

    test TutorialsDaoSQL

--- Codice/server/src/server/dao/TutorialsDaoSQLTest.java
+++ Codice/server/src/server/dao/TutorialsDaoSQLTest.java
@@ -6,12 +6,14 @@
 * Versione: 1.0
 *
 * Modifiche:
-* +---------+---------------+--------------------------+
-* | Data    | Programmatore |         Modifiche        |
-* +---------+---------------+--------------------------+
-* |  130306 |     ZHP       | + creazione documento	   |
-* |         |               |                          |
-* +---------+---------------+--------------------------+
+* +---------+---------------+-------------------------------------------+
+* | Data    | Programmatore |         Modifiche        					|
+* +---------+---------------+-------------------------------------------+
+* |  130306 |     ZHP       | + creazione documento	   					|
+* |  130712 |     VF        | + modificata inizializzazione dei test	|
+* |         |               | + creato testGetTutorialsFromDB          	|
+* |         |               |                    					    |
+* +---------+---------------+-------------------------------------------+
 *
 */ 
 
@@ -19,30 +21,45 @@ package server.dao;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.*;
 
 public class TutorialsDaoSQLTest {
-	 private JavaConnectionSQLite connection;
-	 private TutorialsDaoSQL tutorialsDaoSQL;
-	 
-	 @Test
-	 public void testTutorialsDaoSQL() {
-		connection= new JavaConnectionSQLite();
+	private TutorialsDaoSQL tutorialsDaoSQL;
+	private JavaConnectionSQLite connection = JavaConnectionSQLite.getInstance();
+	ResultSet rs;
+	
+	@Before
+	public void init(){
 		
-		//TutorialDataSQL vuoto
-		tutorialsDaoSQL=new  TutorialsDaoSQL(connection);
-		assertTrue("",tutorialsDaoSQL.getTutorials().getTutorials().size()==0);
-		
-		//TutorialDataSQL non vuoto
+		connection.executeUpdate("DELETE FROM TutorialDataSQL");
 		connection.executeUpdate("INSERT INTO TutorialDataSQL VALUES ('url1','title1');");
 		connection.executeUpdate("INSERT INTO TutorialDataSQL VALUES ('url2','title2');");
 		connection.executeUpdate("INSERT INTO TutorialDataSQL VALUES ('url3','title3');");
-		tutorialsDaoSQL=new  TutorialsDaoSQL(connection);
+		tutorialsDaoSQL=TutorialsDaoSQL.getInstance();
+			
+	}
+	 
+	@Test
+	public void testGetTutorialsFromDB() throws SQLException {
 		
-		System.out.println(tutorialsDaoSQL.getTutorials().getTutorials().size());
-		assertTrue("",tutorialsDaoSQL.getTutorials().getTutorials().size()==3);
+		rs = connection.select("TutorialDataSQL","*","","");
+		int cont=0;
+		if(rs!=null){
+			do{
+				 cont++;
+			}while( rs.next());
+		}
+		assertTrue("Numero messaggi errato",cont==3);
 		
-		connection.executeUpdate("DELETE FROM TutorialDataSQL");
+		assertTrue("Numero tutorial errato",tutorialsDaoSQL.getTutorials().getTutorials().size()==3);
+	}
+	 
+	@Test
+	public void testGetInstance() {
+		assertTrue("Rimozione riuscita",tutorialsDaoSQL.equals(TutorialsDaoSQL.getInstance()));
 	}
 
 }
