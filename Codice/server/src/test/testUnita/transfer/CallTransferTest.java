322c45cba1058bf6ef8fae346aafd69b2cfe1d2f
vfurlan <valentino_furlan@yahoo.it>
2013-07-17 18:08:37 +0200

    test

---
+++ Codice/server/src/test/testUnita/transfer/CallTransferTest.java
@@ -0,0 +1,14 @@
+package test.testUnita.transfer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CallTransferTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
